00bc3a082dd83a375f6ec9fb528656891aa3aa2d
Ash Logan <ash@heyquark.com>
2019-07-15 16:24:35 +1000

    bsp: a bit more hw versioning stuff
    This is actually the worst

--- ios_bsp/bsp_main.c
+++ ios_bsp/bsp_main.c
@@ -213,6 +213,8 @@ unsigned int init_stuff() {
     return ret;
 }
 
+// now for hardware versioning: this bit doesn't make much sense yet
+
 unsigned int dword_E6047204; //.bss:E6047204
 unsigned int arr_E6047208[0x80]; //.bss:E6047208
 unsigned int wood_hardware_ver; //.bss:E6047984
@@ -230,7 +232,7 @@ int sub_E6000CC4() {
     *LT_RESETS_COMPAT |= 0xD0000; //acc. to WiiBrew, this is DDR2, I and DDR1
     *LT_EXICTRL = (*LT_EXICTRL & ~1) | 1; //enable EXI
 
-    error  = sub_E600B5B4(&wood_hardware_ver); //.text:E600B5B4 - see below
+    error  = InitASICVersion(&wood_hardware_ver); //.text:E600B5B4 - see below
     error |= sub_E6006800(); //see below
 
     wood_hardware_ver = 0; //are you kidding me, Nintendo?
@@ -243,6 +245,14 @@ int sub_E6006800() {
     unsigned int woodver;
 
     ret = sub_E600B62C(&woodver); //.text:E600B62C - see below
+    if (ret == 0) {
+        if ((woodver & 0xF0000000) != 0) {
+            return sub_E6000B60(/* &something */);
+            //something: &"GPIO", 1, 1, 1, &&"IOStrength", 0x90000000, 0, 1
+            //TODO probably a BSP entity
+        }
+        return 0x800;
+    }
     //TODO I'm sick of hardware versioning
 }
 
@@ -266,22 +276,21 @@ int sub_E600B62C(unsigned int *woodver) {
 }
 
 /*  Something to do with getting the Wood version?
+*   .text:e600b5b4
 */
-int sub_E600B5B4(unsigned int* woodver) {
+int InitASICVersion(unsigned int* woodver) {
     int ret;
 
     *woodver = wood_hardware_ver;
-    if (!wood_hardware_ver) return 0;
+    if (wood_hardware_ver) return 0;
 
     ret = GetWoodHardwareVersion(woodver, false); //.text:E600B41C - see below
     if (ret) return ret;
 
     if (*woodver == BSP_HARDWARE_VERSION_BOLLYWOOD_PROD_FOR_WII) {
-        if (sub_E600B328() == 0) {
+        if (GetLatteHardwareVersion(woodver) == 0) {
             *woodver = *woodver & 0xFFFF0000 | 0x11;
-         /* aka:
-            *arg1 = 0x20100011;
-            This is not a documented hardware revision... */
+         /* This appears to force a EV_Y board? */
         }
     }
     return 0;
@@ -294,7 +303,6 @@ unsigned short word_E6047988; //.bss:E6047988. IDA reckons it's a byte.
     Doesn't seem to be an ASICREV_ACR revision for HOLLYWOOD_PROD_FOR_WII or
     HOLLYWOOD_CORTADO_ESPRESSO, this is determined from other factors */
 int GetWoodHardwareVersion(unsigned int* woodver, bool doExtraHardwareVersions) {
-    unsigned int isHollywoodProdForWii;
     int ret;
 
     switch (*LT_ASICREV_ACR & 0xFF /*VERLO: revision*/) {
@@ -310,17 +318,17 @@ int GetWoodHardwareVersion(unsigned int* woodver, bool doExtraHardwareVersions) 
             *woodver = BSP_HARDWARE_VERSION_CORTADO;
             if (!doExtraHardwareVersions) return 0;
 
-            stupidstackvar = 0;
-            ret = CheckHollywoodProdForWii(&isHollywoodProdForWii); //.text:E6005818
+            unsigned int isCortado;
+            ret = CheckCortado(&isCortado); //.text:E6005818
             if (ret) return 0;
 
-            if (stupidstackvar == 0) {
+            if (!isCortado) {
                 *woodver = BSP_HARDWARE_VERSION_HOLLYWOOD_PROD_FOR_WII;
                 return 0;
             }
 
             sub_E6007C5C(&word_E6047988);
-            *woodver = BSP_HARDWARE_VERSION_CORTADO; //yes, really (???)
+            *woodver = BSP_HARDWARE_VERSION_CORTADO;
             if (word_E6047988 == 0x7001) {
                 *woodver = BSP_HARDWARE_VERSION_CORTADO_ESPRESSO;
             }
@@ -338,5 +346,41 @@ int GetWoodHardwareVersion(unsigned int* woodver, bool doExtraHardwareVersions) 
             *woodver = BSP_HARDWARE_VERSION_UNKNOWN;
             return 0x800;
         }
+    }
+}
+
+//.text:E600B328
+int GetLatteHardwareVersion(unsigned int* woodver) {
+    int ret;
+
+    uint32_t asicrev_ccr = *LT_ASICREV_CCR;
+    if (asicrev_ccr & 0xFFFF0000 != 0xCAFE0000) {
+        return 0x800;
+    }
+
+    *woodver &= 0x000FFFFF;
+
+    switch (asicrev_ccr & 0xFF) /* VERLO */ {
+        case 0x10:
+            *woodver |= 0x21100000; //Latte A11
+            return 0;
+        case 0x18:
+            *woodver |= 0x21200000; //Latte A12
+            return 0;
+        case 0x21:
+            *woodver |= 0x22100000; //Latte A2x
+            return 0;
+        case 0x30:
+            *woodver |= 0x23100000; //Latte A3x
+            return 0;
+        case 0x40:
+            *woodver |= 0x24100000; //Latte A4x
+            return 0;
+        case 0x50:
+            *woodver |= 0x25100000; //Latte A5x
+            return 0;
+        default:
+            *woodver |= 0x26100000; //Latte B1x
+            return 0;
     }
 }
