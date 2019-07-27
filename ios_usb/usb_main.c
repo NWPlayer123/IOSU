ea998daf21f2fe93f0a1381b5c848e65fa6c7f0a
Ash Logan <ash@heyquark.com>
2019-07-27 20:46:32 +1000

    usb: wonky error math, cleanup

--- ios_usb/usb_main.c
+++ ios_usb/usb_main.c
@@ -1,58 +1,78 @@
-uint32_t[2] default_uptime = { 0, 0 }; //.rodata:101403F8
+static uint32_t[2] default_uptime = { 0, 0 }; //.rodata:101403F8
+static int32_t logHandle = -1; //.data:10145000, written by .text:101001c0
 
-void __startRootHubs(int options) { //TODO: figure out that wonky math for error status
+// This compiled to some heckin wonky math in the original binary.
+// Goal here is to have the same semantics, not to be instruction-perfect.
+#define STATUS_MAKE(hi, lo) (((uint16_t)hi << 16) | (uint16_t)lo)
+#define STATUS_LO(x) (x & 0xFFFF)
+#define STATUS_HI(x) ((x >> 16) & 0xFFFF)
+
+//.text:10100000 (note: not entrypoint!)
+int32_t __startRootHubs(int options) {
 	uint32_t[4] errlist; //SP+4-0x14
 	uint32_t[2] uptime; //SP+0x14, +0x18
+	int32_t status, ret;
+
 	//no idea why they do it like this
-	memcpy(&uptime, &dword_101403F8, sizeof(uptime));
-	IOS_GetUpTimeStruct(&uptime);
-	sub_1012CD80(dword_10145000, 3, "Activating root hubs @ uptime %d.%03d s with options 0x%x.\n", uptime[1], uptime[0] / 1000, options);
-	ret = UhsServerAddHc(dword_10145020, 0, options);
-	if ( ret >= 0 ) { //negative = error
-		sub_1012CD80(dword_10145000, 0, "%s OK.\n", "UhsServerAddHc 0");
-		ret = UhsServerAddHc(dword_10145020, 1, options);
-		if ( ret >= 0 ) { //negative = error
-			sub_1012CD80(dword_10145000, 0, "%s OK.\n", "UhsServerAddHc 1");
+	memcpy(&uptime, &default_uptime, sizeof(default_uptime));
+	IOS_GetUpTimeStruct(&uptime); //UND #0x190
+	USBVReport(logHandle, 3, "Activating root hubs @ uptime %d.%03d s with options 0x%x.\n", uptime[1], uptime[0] / 1000, options); //.text:1012CD80, looks a lot like COSVReport
+
+	ret = UhsServerAddHc(dword_10145020, 0, options); //.text:1010fd28
+	if ( ret < 0 ) { //negative = error
+		if ( STATUS_LO(ret) < 0 ) {
+			status = STATUS_MAKE(-1, STATUS_LO(ret));
 		} else {
-			if ( ret & 0x8000 )
-				v7 = ~(ret << 16);
-			else
-				v7 = ret << 16;
-			if ( ret & 0x8000 )
-				v8 = ~(v7 >> 16);
-			else
-				v8 = v7 >> 16;
-			sub_1012CD80(
-				dword_10145000,
-				2,
-				"%s, file %s, function %s, line %d, status %d:%d\n",
-				"UhsServerAddHc 1",
-				"usb_main.c",
-				"__startRootHubs",
-				0x176,
-				(~ret >> 16) & 0x3FF,
-				v8);
-		} //fall-through
-	} else {
-		if ( ret & 0x8000 )
-			v5 = ~(ret << 16);
-		else
-			v5 = ret << 16;
-		if ( ret & 0x8000 )
-			v6 = ~(v5 >> 16);
-		else
-			v6 = v5 >> 16;
-		sub_1012CD80(
-			dword_10145000,
-			2,
+			status = STATUS_MAKE(0, STATUS_LO(ret));
+		}
+		USBVReport(
+			logHandle, 2,
 			"%s, file %s, function %s, line %d, status %d:%d\n",
 			"UhsServerAddHc 0",
+#ifdef ACCURATE
 			"usb_main.c",
 			"__startRootHubs",
-			0x173,
-			(~ret >> 16) & 0x3FF,
-			v6);
-	} //fall-through, no return(?)
+			371,
+#else
+			__FILE__,
+			__FUNCTION__,
+			__LINE__,
+#endif
+			(~err >> 16) & 0x3FF, err);
+	} else {
+		USBVReport(logHandle, 0, "%s OK.\n", "UhsServerAddHc 0");
+	}
+
+	ret = UhsServerAddHc(dword_10145020, 1, options);
+	if ( ret < 0 ) { //negative = error
+		/* The original code for this isn't nearly as nice, presumably there
+		   was some struct or other thing involved originally */
+		if ( STATUS_LO(ret) < 0 ) {
+			status = STATUS_MAKE(-1, STATUS_LO(ret));
+		} else {
+			status = STATUS_MAKE(0, STATUS_LO(ret));
+		}
+
+		USBVReport(
+			logHandle,
+			2,
+			"%s, file %s, function %s, line %d, status %d:%d\n",
+			"UhsServerAddHc 1",
+#ifdef ACCURATE
+			"usb_main.c",
+			"__startRootHubs",
+			374,
+#else
+			__FILE__,
+			__FUNCTION__,
+			__LINE__,
+#endif
+			(~status >> 16) & 0x3FF,
+			status);
+	} else {
+		USBVReport(logHandle, 0, "%s OK.\n", "UhsServerAddHc 1");
+	}
+
 }
 
 sub_1012CE60(R0, R1) {
